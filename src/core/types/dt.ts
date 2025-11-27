73672c14620a1ed1861186831a9ba1a460d9e71b
danielmawasha <dmawasha@chieta.org.za>
2025-11-27 19:55:16 +0200

    Create dt.ts

---
+++ src/core/types/dt.ts
@@ -0,0 +1,168 @@
+interface ApplicationRecord {
+  id: number;
+  ApprovalStatus: string | null;
+  Number_Continuing: number;
+  Number_New: number;
+  GEC_New: number;
+  GEC_Continuing: number;
+  GAC_New: number;
+  GAC_Continuing: number;
+  GC_New: number;
+  GC_Continuing: number;
+  Female: number;
+  Youth: number;
+  Province: string;
+}
+
+[
+  {
+    "id": 2042,
+    "ApprovalStatus": null,
+    "Number_Continuing": 0,
+    "Number_New": 6,
+    "GEC_New": 6,
+    "GEC_Continuing": 0,
+    "GAC_New": 6,
+    "GAC_Continuing": 0,
+    "GC_New": 6,
+    "GC_Continuing": 0,
+    "Female": 5,
+    "Youth": 6,
+    "Province": "Gauteng"
+  },
+  {
+    "id": 2043,
+    "ApprovalStatus": null,
+    "Number_Continuing": 0,
+    "Number_New": 5,
+    "GEC_New": 2,
+    "GEC_Continuing": 0,
+    "GAC_New": 2,
+    "GAC_Continuing": 0,
+    "GC_New": 2,
+    "GC_Continuing": 0,
+    "Female": 1,
+    "Youth": 2,
+    "Province": "Mpumalanga"
+  },
+  {
+    "id": 2044,
+    "ApprovalStatus": "Approved",
+    "Number_Continuing": 0,
+    "Number_New": 5,
+    "GEC_New": 5,
+    "GEC_Continuing": 0,
+    "GAC_New": 5,
+    "GAC_Continuing": 0,
+    "GC_New": 5,
+    "GC_Continuing": 0,
+    "Female": 4,
+    "Youth": 5,
+    "Province": "Gauteng"
+  },
+  {
+    "id": 2045,
+    "ApprovalStatus": null,
+    "Number_Continuing": 0,
+    "Number_New": 5,
+    "GEC_New": 5,
+    "GEC_Continuing": 0,
+    "GAC_New": 5,
+    "GAC_Continuing": 0,
+    "GC_New": 5,
+    "GC_Continuing": 0,
+    "Female": 3,
+    "Youth": 5,
+    "Province": "Gauteng"
+  },
+  {
+    "id": 2046,
+    "ApprovalStatus": null,
+    "Number_Continuing": 0,
+    "Number_New": 1,
+    "GEC_New": 1,
+    "GEC_Continuing": 0,
+    "GAC_New": 1,
+    "GAC_Continuing": 0,
+    "GC_New": 1,
+    "GC_Continuing": 0,
+    "Female": 0,
+    "Youth": 1,
+    "Province": "Gauteng"
+  },
+  {
+    "id": 2047,
+    "ApprovalStatus": null,
+    "Number_Continuing": 0,
+    "Number_New": 5,
+    "GEC_New": 0,
+    "GEC_Continuing": 2,
+    "GAC_New": 0,
+    "GAC_Continuing": 2,
+    "GC_New": 0,
+    "GC_Continuing": 2,
+    "Female": 0,
+    "Youth": 5,
+    "Province": "Mpumalanga"
+  },
+  {
+    "id": 2048,
+    "ApprovalStatus": "Approved",
+    "Number_Continuing": 5,
+    "Number_New": 0,
+    "GEC_New": 0,
+    "GEC_Continuing": 5,
+    "GAC_New": 0,
+    "GAC_Continuing": 5,
+    "GC_New": 0,
+    "GC_Continuing": 5,
+    "Female": 3,
+    "Youth": 5,
+    "Province": "Gauteng"
+  },
+  {
+    "id": 2050,
+    "ApprovalStatus": "Approved",
+    "Number_Continuing": 10,
+    "Number_New": 0,
+    "GEC_New": 0,
+    "GEC_Continuing": 10,
+    "GAC_New": 0,
+    "GAC_Continuing": 10,
+    "GC_New": 0,
+    "GC_Continuing": 10,
+    "Female": 8,
+    "Youth": 10,
+    "Province": "Gauteng"
+  },
+  {
+    "id": 2051,
+    "ApprovalStatus": "Approved",
+    "Number_Continuing": 10,
+    "Number_New": 0,
+    "GEC_New": 0,
+    "GEC_Continuing": 10,
+    "GAC_New": 0,
+    "GAC_Continuing": 10,
+    "GC_New": 0,
+    "GC_Continuing": 10,
+    "Female": 8,
+    "Youth": 10,
+    "Province": "Gauteng"
+  },
+  {
+    "id": 2052,
+    "ApprovalStatus": "Approved",
+    "Number_Continuing": 5,
+    "Number_New": 0,
+    "GEC_New": 0,
+    "GEC_Continuing": 5,
+    "GAC_New": 0,
+    "GAC_Continuing": 5,
+    "GC_New": 0,
+    "GC_Continuing": 5,
+    "Female": 3,
+    "Youth": 5,
+    "Province": "Gauteng"
+  }
+]
