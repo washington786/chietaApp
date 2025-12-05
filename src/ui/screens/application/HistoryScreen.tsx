6a1e49d75bd90aa35c7a7d526a1a78cc6ba7123e
washington786 <dkmawasha@gmail.com>
2025-12-05 22:38:08 +0200

    feat: added animation to history screen ui

--- src/ui/screens/application/HistoryScreen.tsx
+++ src/ui/screens/application/HistoryScreen.tsx
@@ -1,31 +1,37 @@
-import { FlatList, StyleSheet } from 'react-native'
+import { FlatList, StyleSheet, View } from 'react-native'
 import React from 'react'
 import { RCol, SafeArea } from '@/components/common'
 import { Text } from 'react-native-paper'
 import colors from '@/config/colors'
 import { AppTrackingItem } from '@/components/modules/application'
 import usePageTransition from '@/hooks/navigation/usePageTransition'
+import Animated, { FadeInDown } from 'react-native-reanimated'
 
 const HistoryScreen = () => {
     const { historyItemDetails } = usePageTransition();
+
+    const renderList = ({ index, item }: { index: number, item: any }) => {
+        return (
+            <Animated.View key={`tracking-${item}-${Date.now()}`} entering={FadeInDown.duration(600).delay(index * 100).springify()}>
+                <AppTrackingItem onPress={() => historyItemDetails({ appId: index.toString() })} />
+            </Animated.View>
+        )
+    }
     return (
         <SafeArea>
-            <FlatList data={[]}
+            <FlatList data={[1, 2, 3, 4, 5]}
+                keyExtractor={(item) => item.toString()}
                 style={{ paddingHorizontal: 12, paddingVertical: 5, flex: 1, flexGrow: 1 }}
-                renderItem={null}
+                renderItem={renderList}
                 ListHeaderComponent={<RCol style={styles.conWrap}>
                     <Text variant='titleMedium' style={styles.textColor}>application tracking status</Text>
                 </RCol>}
-                ListFooterComponent={() => {
-                    return (
-                        <>
-                            <AppTrackingItem onPress={() => historyItemDetails({ appId: "2" })} />
-                            <AppTrackingItem onPress={() => historyItemDetails({ appId: "2" })} />
-                            <AppTrackingItem onPress={() => historyItemDetails({ appId: "2" })} />
-                            <AppTrackingItem onPress={() => historyItemDetails({ appId: "2" })} />
-                        </>
-                    )
-                }}
+                showsVerticalScrollIndicator={false}
+                ItemSeparatorComponent={() => <View style={{ height: 5 }} />}
+                removeClippedSubviews={false}
+                initialNumToRender={10}
+                maxToRenderPerBatch={10}
+                windowSize={21}
             />
         </SafeArea>
     )
