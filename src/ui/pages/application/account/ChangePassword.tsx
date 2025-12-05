c3b864b12dd133d7f93f7ee824a0e7e5ab7e4b56
washington786 <dkmawasha@gmail.com>
2025-12-05 23:21:26 +0200

    change password.

--- src/ui/pages/application/account/ChangePassword.tsx
+++ src/ui/pages/application/account/ChangePassword.tsx
@@ -1,5 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet } from 'react-native'
 import RHeader from '@/components/common/RHeader'
 import { RButton, RInput, Scroller } from '@/components/common'
 import Animated, { FadeInDown } from 'react-native-reanimated'
