1ff079d44a818376f9f5cae8097b56d30c47cc7b
washington786 <dkmawasha@gmail.com>
2026-01-24 13:32:18 +0200

    feat: implemented mandatory grant applications search

--- src/ui/pages/application/MandatoryPage.tsx
+++ src/ui/pages/application/MandatoryPage.tsx
@@ -1,9 +1,9 @@
 import { FlatList, StyleSheet, View } from 'react-native'
 import React, { useEffect, useMemo, useState } from 'react'
-import { REmpty, RListLoading, SafeArea } from '@/components/common'
+import { RCol, REmpty, RListLoading, SafeArea } from '@/components/common'
 import RHeader from '@/components/common/RHeader'
 import { ApplicationItem, InformationBanner } from '@/components/modules/application'
-import { FAB } from 'react-native-paper'
+import { FAB, Searchbar } from 'react-native-paper'
 import usePageTransition from '@/hooks/navigation/usePageTransition'
 import colors from '@/config/colors'
 import Animated, { FadeInDown } from 'react-native-reanimated';
@@ -16,6 +16,10 @@ import { useGetOrgApplicationsQuery } from '@/store/api/api'
 const MandatoryPage = () => {
     const { newApplication } = usePageTransition();
     const [allApplications, setAllApplications] = useState<MandatoryApplicationDto[]>([]);
+
+    const [searchQuery, setSearchQuery] = useState<string>('');
+
+    const [show, setShow] = useState<boolean>(false);
 
     const router = useRoute<RouteProp<navigationTypes, "mandatory">>();
 
@@ -47,6 +51,24 @@ const MandatoryPage = () => {
         return [...allApplications].sort((a, b) => b.id - a.id);
     }, [allApplications]);
 
+    const fieldsToSearch = ['referenceNo', 'description', 'grantStatus', 'organisation_Name'] as const;
+
+    const filteredApplications = useMemo(() => {
+        let applications = sortedApplications;
+
+        if (searchQuery?.trim()) {
+            const query = searchQuery.trim().toLowerCase();
+            applications = sortedApplications.filter((app) => {
+                return fieldsToSearch.some((field) => {
+                    const value = app[field];
+                    return typeof value === 'string' && value.toLowerCase().includes(query);
+                });
+            });
+        }
+
+        return applications;
+    }, [sortedApplications, searchQuery]);
+
     const renderList = ({ index, item }: { index: number, item: MandatoryApplicationDto }) => {
         return (
             <Animated.View key={`app-${item.id}}`} entering={FadeInDown.duration(600).delay(index * 100).springify()}>
@@ -57,29 +79,51 @@ const MandatoryPage = () => {
 
     if (loading) {
         return <RListLoading count={7} />
-    } else {
-        return (
-            <SafeArea>
-                <RHeader name='Mandatory Grant Applications' />
-                <FlatList data={sortedApplications}
-                    style={{ paddingHorizontal: 12, paddingVertical: 6, flex: 1, flexGrow: 1 }}
-                    renderItem={renderList}
-                    ListHeaderComponent={<InformationBanner title='view your applications and apply for new grants. You can only submit during open grant window.' />}
-                    showsVerticalScrollIndicator={false}
-                    ItemSeparatorComponent={() => <View style={{ height: 5 }} />}
-                    removeClippedSubviews={false}
-                    ListEmptyComponent={<REmpty title='No Applications Found' subtitle={`when you have applications, they'll appear here`} />}
-                />
-                <FAB
-                    mode='flat'
-                    icon="plus"
-                    style={styles.fab}
-                    onPress={newApplication}
-                    color='white'
-                />
-            </SafeArea>
-        )
     }
+    return (
+        <SafeArea>
+            <RHeader name='Mandatory Grant Applications' hasRightIcon iconRight='search' onPressRight={() => setShow(!show)} />
+            {
+                show &&
+                <RCol style={{
+                    paddingVertical: 6,
+                    paddingHorizontal: 12
+                }}>
+                    <Searchbar
+                        placeholder="Search application"
+                        onChangeText={setSearchQuery}
+                        value={searchQuery}
+                        style={{ backgroundColor: colors.zinc[50], borderWidth: 1, borderColor: colors.zinc[300] }}
+                    />
+                </RCol>
+            }
+            <FlatList data={filteredApplications}
+                style={{ paddingHorizontal: 12, paddingVertical: 6, flex: 1, flexGrow: 1 }}
+                renderItem={renderList}
+                ListHeaderComponent={
+                    <>
+                        {
+                            !show &&
+                            <InformationBanner title='search and filter through your mandatory grant applications.' />
+                        }
+
+                    </>
+                }
+                showsVerticalScrollIndicator={false}
+                ItemSeparatorComponent={() => <View style={{ height: 5 }} />}
+                removeClippedSubviews={false}
+                ListEmptyComponent={<REmpty title='No Applications Found' subtitle={`when you have applications, they'll appear here`} />}
+            />
+            <FAB
+                mode='flat'
+                icon="plus"
+                style={styles.fab}
+                onPress={newApplication}
+                color='white'
+            />
+        </SafeArea>
+    )
+
 
 }
 
