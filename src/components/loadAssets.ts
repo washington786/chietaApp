a63395e3b195845e9255e202fa3850fc96f9d0b2
washington786 <dkmawasha@gmail.com>
2026-02-02 11:53:47 +0200

    loaded-assets

--- src/components/loadAssets.ts
+++ src/components/loadAssets.ts
@@ -3,4 +3,5 @@ import { ImageSourcePropType } from "react-native";
 const errorBox: ImageSourcePropType = require('../../assets/error-box.png');
 const errorInspect: ImageSourcePropType = require('../../assets/error-inspect-v1.png');
 const chatSquare: ImageSourcePropType = require('../../assets/chat-square.png');
-export { errorBox, errorInspect, chatSquare };
+const chat: ImageSourcePropType = require('../../assets/smartphone-chat.png');
+export { errorBox, errorInspect, chatSquare, chat };
